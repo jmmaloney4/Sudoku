74fbe7f58c3f96cf84a8c89d08732c1239d297ad
Jack Maloney <jmmaloney4@gmail.com>
2014-03-14 17:49:30 -0500

    grabbed the UI class used before

--- src/com/four/sudoku/Sudoku.java
+++ src/com/four/sudoku/Sudoku.java
@@ -1,26 +1,41 @@
 package com.four.sudoku;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Sudoku {
 	
 	static Scanner scanner;
 	static Puzzle puzzle = new Puzzle();
-	static MainUI UI = new MainUI();
+	static UIMgr UI = new UIMgr();
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+		
+		try {
+			UI.Start();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		if (args.length < 1) {
 			System.out.println("Not Enough Args");
-			//System.exit(1);
+			UI.AutoExit(3);
 		}
 		
-		//scanner = new Scanner(args[0]);
-		
-		while(true) {
-			
-		}
-		
+		scanner = new Scanner(new File(args[0]));
 		
 	}
 }
