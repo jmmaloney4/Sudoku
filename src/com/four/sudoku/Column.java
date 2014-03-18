a69152b86f40bb1e486b1e49996a1727d327efe5
Jack Maloney <jmmaloney4@gmail.com>
2014-03-18 14:17:44 -0500

    removed unnecessary classes

--- src/com/four/sudoku/Column.java
+++ src/com/four/sudoku/Column.java
@@ -2,8 +2,6 @@ package com.four.sudoku;
 
 public class Column extends Group {
 
-	protected SColumn[] cols = new SColumn[3];
-
 	public Column(int pos) {
 		super(pos);
 		// TODO Auto-generated constructor stub
@@ -12,14 +10,6 @@ public class Column extends Group {
 	@Override
 	public int getType() {
 		return Group.COLUMN;
-	}
-
-	public SColumn[] getCols() {
-		return cols;
-	}
-
-	public void setCols(SColumn[] cols) {
-		this.cols = cols;
 	}
 
 }
--- src/com/four/sudoku/Row.java
+++ src/com/four/sudoku/Row.java
@@ -2,8 +2,6 @@ package com.four.sudoku;
 
 public class Row extends Group {
 
-	protected SRow[] rows = new SRow[3];
-
 	public Row(int pos) {
 		super(pos);
 		// TODO Auto-generated constructor stub
@@ -12,14 +10,6 @@ public class Row extends Group {
 	@Override
 	public int getType() {
 		return Group.ROW;
-	}
-
-	public SRow[] getRows() {
-		return rows;
-	}
-
-	public void setRows(SRow[] rows) {
-		this.rows = rows;
 	}
 
 }
